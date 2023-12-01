c02dd40df21422e81937e141253a0c9c053629a3
BennettAnderson7 <142930353+BennettAnderson7@users.noreply.github.com>
2023-12-01 11:43:55 -0800

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,106 @@
+import pygame as pg
+from pygame.sprite import Sprite
+
+from pygame.math import Vector2 as vec
+import os
+from settings import *
+
+# setup asset folders here - images sounds etc.
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'images')
+snd_folder = os.path.join(game_folder, 'sounds')
+
+# defining classes:
+
+# player class
+class Player(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self)
+        # self.image = pg.Surface((50, 50))
+        # self.image.fill(GREEN)
+        # use an image for player sprite...
+        self.game = game
+        self.image = pg.image.load(os.path.join(img_folder, 'mario_sprite.png')).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (0, 0)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0) 
+    def controls(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -5
+        if keys[pg.K_d]:
+            self.acc.x = 5
+        if keys[pg.K_SPACE]:
+            self.jump()
+    def jump(self):
+        hits = pg.sprite.spritecollide(self, self.game.all_platforms, False)
+        if hits:
+            print("i can jump")
+            self.vel.y = -PLAYER_JUMP
+    def update(self):
+        self.acc = vec(0,PLAYER_GRAV)
+        self.controls()
+        # if friction - apply here
+        self.acc.x += self.vel.x * -PLAYER_FRIC
+        # self.acc.y += self.vel.y * -0.3
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.midbottom = self.pos
+
+# platform class
+class Platform(Sprite):
+    def __init__(self, x, y, w, h, category):
+        Sprite.__init__(self)
+        # Image change
+        # self.image = pg.Surface((w, h))
+        self.image = pg.image.load(os.path.join(img_folder, 'mario_blockfloor.png')).convert()
+        # self.image.fill(BROWN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.category = category
+        self.speed = 0
+        '''
+        if self.category == "moving":
+            self.speed = 5
+    def update(self):
+        if self.category == "moving":
+            self.rect.x += self.speed
+            if self.rect.x + self.rect.w > WIDTH or self.rect.x < 0:
+                self.speed = -self.speed
+'''
+# block class
+class Block(Sprite):
+    def __init__(self, x, y, w, h):
+        Sprite.__init__(self)
+        '''self.image = pg.Surface((w, h))
+        #self.image.fill(BROWN)'''
+        # input texture
+        self.image = pg.image.load(os.path.join(img_folder, 'mario_qblock.png')).convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 0
+
+# mob class (removed)
+class Mob(Sprite):
+    def __init__(self, x, y, w, h, kind):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.kind = kind
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+
+    # update
+    def update(self):
+        pass
+
+
+        
